3bce1ed78aa8d968f1b66c6a3924d535f535be95
jason0814 <jason0814@iabacus.co.kr>
2022-05-11 16:44:47 +0900

    최소 업로드

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-col uvms">
+    <div class="flex-1 content">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueCompositionApi from "@vue/composition-api";
+
+Vue.use(VueCompositionApi);
+export default {
+  name: "Default",
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+// 레이아웃
+.uvms {
+  height: 100vh;
+  margin: 0 auto;
+  .content {
+    overflow-y: auto;
+    height: calc(100vh - 50px);
+    padding: 30px 20px 20px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>hi test</div>
+</template>
+
+<script>
+import { reactive } from "@vue/composition-api";
+
+export default {
+  name: "index",
+  components: {},
+  setup() {
+    const state = reactive({
+      text: "testTitle",
+    });
+
+    return { state };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
